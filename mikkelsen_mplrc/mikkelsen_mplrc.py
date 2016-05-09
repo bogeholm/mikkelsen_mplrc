4b575f8e14c5ef8d69e1318b17d0b523e11f990d
Troels Bøgeholm Mikkelsen <bogeholm@dhcp0c1.dhcp.gfy.ku.dk>
2016-05-09 16:38:30 +0200

    First commit

---
+++ mikkelsen_mplrc/mikkelsen_mplrc.py
@@ -0,0 +1,24 @@
+import matplotlib as mpl
+
+
+def set_paper():
+    # Matplotlib parameters
+    # Use TeX
+    mpl.rcParams['text.usetex']=True
+    mpl.rcParams['text.latex.unicode']=True
+
+    # https://stackoverflow.com/questions/17958485/matplotlib-not-using-latex-font-while-text-usetex-true
+    #from matplotlib import rc as mplrc
+    #mplrc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+    #mplrc('text', usetex=True)
+    mpl.rcParams['font.family'] = 'serif'
+    mpl.rcParams['font.serif'] = 'Computer Modern'
+
+    # Font size of labels and ticklabels
+    fontsize = 22 # Font size
+    ticksize = 16
+    # Set the above
+    mpl.rcParams['axes.labelsize'] = fontsize
+    mpl.rcParams['legend.fontsize'] = fontsize
+    mpl.rcParams['xtick.labelsize'] = ticksize
+    mpl.rcParams['ytick.labelsize'] = ticksize
