92958ad202ee56d7970783bcb1afb403a10ca366
Troels Bøgeholm Mikkelsen <bogeholm@dhcp0a2.dhcp.gfy.ku.dk>
2016-08-24 17:50:47 +0200

    New colors

--- mikkelsen_mplrc/mikkelsen_mplrc.py
+++ mikkelsen_mplrc/mikkelsen_mplrc.py
@@ -1,4 +1,8 @@
 import matplotlib as mpl
+import numpy as np
+from cycler import cycler
+import matplotlib.pyplot as plt
+
 #import seaborn as sns
 
 def paper():
@@ -62,3 +66,49 @@ def presentation():
 
 def mpldefaults():
     mpl.rcdefaults()
+
+# Color options
+def sRGB():
+    # Define Apple colors - using sRGB
+    blue = np.array([18, 187, 250]) / 255
+    pink = np.array([255, 71, 123]) / 255
+    orange = np.array([255, 163, 2]) / 255
+    green = np.array([113, 221, 70]) / 255
+    yellow = np.array([255, 211, 2]) / 255
+    purple = np.array([170, 130, 255]) / 255
+    black = np.array([0, 0, 0]) / 255
+
+    return [blue, pink, orange, green, yellow, purple, black]
+
+def genericRGB():
+    # Define Apple colors - using generic RGB
+    blue = np.array([27, 172, 248]) / 255
+    pink = np.array([251, 42, 104]) / 255
+    orange = np.array([252, 146, 10]) / 255
+    green = np.array([99, 219, 55]) / 255
+    yellow = np.array([254, 204, 11]) / 255
+    purple = np.array([153, 102, 255]) / 255
+    black = np.array([0, 0, 0]) / 255
+
+    return [blue, pink, orange, green, yellow, purple, black]
+
+def adobeRGB():
+    # Define Apple colors - using Adobe RGB
+    blue = np.array([106, 185, 248]) / 255
+    pink = np.array([221, 73, 121]) / 255
+    orange = np.array([223, 162, 38]) / 255
+    green = np.array([153, 220, 85]) / 255
+    yellow = np.array([243, 210, 50]) / 255
+    purple = np.array([159, 129, 251]) / 255
+    black = np.array([0, 0, 0]) / 255
+
+    return [blue, pink, orange, green, yellow, purple, black]
+
+def set_sRGB():
+    plt.rc('axes', prop_cycle=(cycler('color', sRGB())))
+
+def set_genericRGB():
+    plt.rc('axes', prop_cycle=(cycler('color', genericRGB())))
+
+def set_adobeRGB():
+    plt.rc('axes', prop_cycle=(cycler('color', adobeRGB())))
